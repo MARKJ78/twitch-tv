28ee5994d340fc681b9002aee5a11b31445a385b
Mark Jones <mpjonesemail@gmail.com>
2016-10-04 11:35:03 +0100

    debug

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-//client ida59qej09oftmvj165yc0tnhll3sxps
-
-/*$.ajax({
-    type: 'GET',
-    url: 'https://api.twitch.tv/kraken/channels/twitch',
-    headers: {
-        'Client-ID': 'ida59qej09oftmvj165yc0tnhll3sxps'
-    },
-    success: function(data) {
-        console.log(data);
-    }
-});*/
-//client ida59qej09oftmvj165yc0tnhll3sxps
-
 function callChannels() {
     var userChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     for (var i = 0; i < userChannels.length; i++) {
@@ -40,3 +26,16 @@ function fetch(channel) {
 
 callChannels();
 console.log(channelData);
+
+
+
+/*$.ajax({
+    type: 'GET',
+    url: 'https://api.twitch.tv/kraken/channels/twitch',
+    headers: {
+        'Client-ID': 'ida59qej09oftmvj165yc0tnhll3sxps'
+    },
+    success: function(data) {
+        console.log(data);
+    }
+});*/
