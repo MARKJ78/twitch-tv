eb8ed1e3dece86f58b11964d2fe67cf1fae86c54
Mark Jones <mpjonesemail@gmail.com>
2016-10-05 13:12:13 +0100

    dev

--- js/main.js
+++ js/main.js
@@ -1,47 +1,96 @@
-function fetch(channel) {
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.twitch.tv/kraken/channels/' + channel,
-        headers: {
-            'Client-ID': 'a59qej09oftmvj165yc0tnhll3sxps'
-        },
-        success: function(response) {
-            $('#userCards').append('<div id="user-card-' + response.display_name + '"><div class="status">' + response.status + '</div><div class="logo"><img src="' + response.logo + '" alt="Channel Logo"></div><div class="display-name"><a href="' + response.url + '">' + response.display_name + '</a></div><div class="game"><p>Plyaing:&nbsp;' + response.game + '<p><div></div>');
-            /*  $('#user-card-' + response.display_name).html('<div class="status">' + response.status + '</div><div class="logo"><img src="' + response.logo + '" alt="Channel Logo"></div><div class="display-name"><a href="' + response.url + '">' + response.display_name + '</a></div><div class="game"><p>Plyaing:&nbsp;' + response.game + '<p><div>');*/
-            console.log(response);
+$(document).ready(function() {
 
-        },
-        error: function(error) {
-            console.log(error);
-        },
-    });
-}
+    /*////////////////////////////////////////
 
-function callChannels() {
-    var userChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-    for (var i = 0; i < userChannels.length; i++) {
-        fetch(userChannels[i]);
+
+    Call API
+
+
+    ////////////////////////////////////////*/
+
+    function fetch(channel, name) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.twitch.tv/kraken/streams/' + channel,
+            headers: {
+                'Client-ID': 'a59qej09oftmvj165yc0tnhll3sxps'
+            },
+            success: function(data) {
+                populate(data);
+                console.log(data);
+            },
+            error: function(error) {
+                console.log(error);
+            },
+        });
+    }
+
+    function callChannels() {
+        var userChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas" /*, "brunofin", "comster404"*/ ];
+        for (var i = 0; i < userChannels.length; i++) {
+            fetch(userChannels[i], userChannels[i]);
+        }
+    }
+
+    callChannels();
+
+    console.log("ready!");
+});
+
+/*////////////////////////////////////////
+
+
+Populate
+
+
+////////////////////////////////////////*/
+
+function populate(response) {
+    if (response.stream !== null) {
+        $('#userCards').append('<div class="user-card" id="' + response.stream.channel.display_name + '"><div class="status">' + response.stream.channel.status + '</div><div id="videoPlay-' + response.stream.channel.display_name + '" class="video-play"><i class="fa fa-play" aria-hidden="true"></i></div><div id="videoKill-' + response.stream.channel.display_name + '" class="video-kill"><i class="fa fa-stop" aria-hidden="true"></i></div><div class="logo"><img src="' + response.stream.channel.logo + '" alt="Channel Logo"></div><div class="display-name"><a href="' + response.stream.channel.url + '">' + response.stream.channel.display_name + '</a></div><div class="game"><p>Playing:&nbsp;<b>' + response.stream.channel.game + '</b><p><div></div>');
+    } else {
+        var channel = response._links.channel;
+        console.log(response._links.channel);
+        var urlArray = channel.split('/');
+        var urlName = urlArray[urlArray.length - 1];
+        $('#offlineCards').append('<div class="offline-card"><h2>' + urlName + '&nbsp; is OFFLINE</h2><div class="link"><a href="https://www.twitch.tv/' + urlName + '">Go To Channel&nbsp;<i class="fa fa-external-link fa-lg" aria-hidden="true"></i></a></div>');
+    }
+
+
+
+    /*////////////////////////////////////////
+
+
+    Embeded video function
+
+
+    ////////////////////////////////////////*/
+
+    function getStream(name, streamWidth) {
+        console.log(name);
+        console.log(streamWidth);
+        $('#' + name).append('<div class="user-stream"><iframe src="http://player.twitch.tv/?channel={' + name + '}" width="100%" frameborder="0" scrolling="no" allowfullscreen="true"></iframe></div>');
+    }
+    if (response.stream !== null) {
+        $('#videoPlay-' + response.stream.channel.display_name).click(function() {
+            var thisName = $(this).parent().attr('id');
+            var thisWidth = $(this).parent().width();
+            getStream(thisName, thisWidth);
+            $('#videoPlay-' + response.stream.channel.display_name).css({
+                'display': 'none'
+            });
+            $('#videoKill-' + response.stream.channel.display_name).css({
+                'display': 'block'
+            });
+        });
+        $('#videoKill-' + response.stream.channel.display_name).click(function() {
+            $('.user-stream').remove();
+            $('#videoPlay-' + response.stream.channel.display_name).css({
+                'display': 'block'
+            });
+            $('#videoKill-' + response.stream.channel.display_name).css({
+                'display': 'none'
+            });
+        });
     }
 }
-
-callChannels();
-
-/*function buildPage(userData) {
-    console.log(userData.logo);
-    $('#userCards').append('<div id="user-card' + userData.display_name + '"></div>');
-    $('.user-card' + userData.display_name).html('<div class="logo"><img src="' + userData.logo + '" alt="Channel Logo"></div><div class="display-name"></div><div class="status"></div><div class="url"><div>');
-}*/
-
-
-
-
-/*$.ajax({
-    type: 'GET',
-    url: 'https://api.twitch.tv/kraken/channels/twitch',
-    headers: {
-        'Client-ID': 'ida59qej09oftmvj165yc0tnhll3sxps'
-    },
-    success: function(data) {
-        console.log(data);
-    }
-});*/
