091d4ad254c226dc69ade605c38e9ee5efbb7d51
Mark Jones <mpjonesemail@gmail.com>
2016-10-05 17:08:04 +0100

    new card layout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-    var userChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas" /*, "brunofin", "comster404"*/ ];
+    var userChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
     /*////////////////////////////////////////
-
-
     Call API
-
-
     ////////////////////////////////////////*/
-
     function fetch(channel, name) {
         $.ajax({
             type: 'GET',
@@ -18,6 +13,7 @@ $(document).ready(function() {
             success: function(data) {
                 populate(data);
                 console.log(data);
+
             },
             error: function(error) {
                 console.log(error);
@@ -31,6 +27,7 @@ $(document).ready(function() {
         }
     }
     callChannels();
+
 });
 
 /*////////////////////////////////////////
@@ -43,7 +40,10 @@ Populate
 
 function populate(response, name) {
     if (response.stream !== null) {
-        $('#userCards').append('<div class="user-card" id="' + response.stream.channel.display_name + '"><div class="status">' + response.stream.channel.status + '</div><div id="videoPlay-' + response.stream.channel.display_name + '" class="video-play"><i class="fa fa-play" aria-hidden="true"></i></div><div id="videoKill-' + response.stream.channel.display_name + '" class="video-kill"><i class="fa fa-stop" aria-hidden="true"></i></div><div class="logo" id="logo-' + response.stream.channel.display_name + '"><img src="' + response.stream.channel.logo + '" alt="Channel Logo"></div><div class="display-name"><a href="' + response.stream.channel.url + '">' + response.stream.channel.display_name + '</a></div><div class="game"><p>Playing:&nbsp;<b>' + response.stream.channel.game + '</b><p><div></div>');
+        $('#userCards').append('<div class="user-card" id="' + response.stream.channel.display_name + '">  <div class="top-row">    <div class="on-air-light"></div>    <div class="on-air-text">On air text</div>     <div class="game"><p>Playing:&nbsp;<b>' + response.stream.channel.game + '</b></p></div>    <div class="display-name"><a href="' + response.stream.channel.url + '">' + response.stream.channel.display_name + '</a></div>  </div>  <div class="middle-row" id="middle-row-' + response.stream.channel.display_name + '">  <div class="logo" id="logo-' + response.stream.channel.display_name + '"><img src="' + response.stream.channel.logo + '" alt="Channel Logo"></div>    <div class="status">' + response.stream.channel.status + '</div>  </div>  <div class="bottom-row">    <div class="fave"><p>Favorite</p><i class="fa fa-heart" aria-hidden="true"></i></div>    <div class="go-channel"><p>Visit Channel</p><i class="fa fa-twitch" aria-hidden="true"></i></div>    <div class="player-control">    <div id="videoPlay-' + response.stream.channel.display_name + '" class="video-play"><p>Stream</p><i class="fa fa-play" aria-hidden="true"></i></div>    <div id="videoKill-' + response.stream.channel.display_name + '" class="video-kill"><p>End</p><i class="fa fa-stop" aria-hidden="true"></i></div></div></div><div>');
+        $('#videoKill-' + response.stream.channel.display_name).css({
+            'display': 'none'
+        });
     } else {
         var channel = response._links.channel;
         var urlArray = channel.split('/');
@@ -52,28 +52,37 @@ function populate(response, name) {
     }
 
     /*////////////////////////////////////////
-
-
     Embeded video function
-
-
     ////////////////////////////////////////*/
 
     function getStream(name) {
-        $('#logo-' + name).html('<div id="' + name + '-user-stream" class="user-stream"><iframe src="https://player.twitch.tv/?channel={' + name + '}" width="100%" frameborder="0" scrolling="no" allowfullscreen="true"></iframe></div>');
+        $('#middle-row-' + name).html('<div id="' + name + '-user-stream" class="user-stream"><iframe src="https://player.twitch.tv/?channel={' + name + '}" width="100%" frameborder="0" scrolling="no" allowfullscreen="true"></iframe></div>');
     }
 
     if (response.stream !== null) {
         $('#videoPlay-' + response.stream.channel.display_name).click(function() {
-            var thisName = $(this).parent().attr('id');
+            var thisName = $(this).parent().parent().parent().attr('id');
             getStream(thisName);
-
+            $(this).css({
+                'display': 'none'
+            });
+            $('#videoKill-' + response.stream.channel.display_name).css({
+                'display': 'block'
+            });
         });
     }
     if (response.stream !== null) {
         $('#videoKill-' + response.stream.channel.display_name).click(function() {
             $('#' + response.stream.channel.display_name + '-user-stream').remove();
-            $('#logo-' + response.stream.channel.display_name).html('<img src="' + response.stream.channel.logo + '" alt="Channel Logo">');
+            $('#middle-row-' + response.stream.channel.display_name).html(' <div class="logo" id="logo-' + response.stream.channel.display_name + '"><img src="' + response.stream.channel.logo + '" alt="Channel Logo"></div>    <div class="status">' + response.stream.channel.status + '</div>');
+            $(this).css({
+                'display': 'none'
+            });
+            $('#videoPlay-' + response.stream.channel.display_name).css({
+                'display': 'block'
+            });
+
+
         });
     }
 }
